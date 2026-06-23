cd9e26a4a9d501e6d5498a48d4081e69f486a7b2
Tshering Dorji <tsheringdorji2805@gmail.com>
2026-06-23 12:15:49 +0700

    ticket

--- WX/ticket.py
+++ WX/ticket.py
@@ -1,20 +1,35 @@
-import sys
+import sys  # Imports the sys module to access command-line arguments
 
 def calculate_ticket_price(age):
-    # --- เขียนโค้ดของนักเรียนในส่วนนี้ / Write your code here ---
-    pass
-    # --------------------------------------------------------
 
+    if age < 12:
+        return 120  # Return 120 Baht
+
+    elif age <= 60:
+        return 200  # Return 200 Baht
+
+    else:
+        return 150  # Return 150 Baht
+
+
+# Main function
 def main():
-    # เปลี่ยนมาเช็ก > 1 และใช้ sys.argv[-1] เพื่อความแม่นยำใน VPL
+
     if len(sys.argv) > 1:
+
         test_age = int(sys.argv[-1])
+
         result = calculate_ticket_price(test_age)
+
         print(result)
+
     else:
         test_age = 25
+
         result = calculate_ticket_price(test_age)
+
         print(f"Age: {test_age} -> Ticket Price: {result} Baht")
+
 
 if __name__ == "__main__":
     main()
